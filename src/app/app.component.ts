04efe2d602b619ce76eca7028251992119f541d0
Ali Lozano <alilozanoc@gmail.com>
2023-06-03 10:33:01 -0500

    Tailwind + Counter example added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'books-app';
+  number = 0;
+
+  increment() {
+    this.number = this.number + 1;
+  }
+
+  reset() {
+    this.number = 0;
+  }
 }
