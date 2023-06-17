1d14bbb1c0afde853b43b65634cf80584b1e592a
Ali Lozano <alilozanoc@gmail.com>
2023-06-17 12:41:51 -0500

    Added token to authentication

--- src/app/services/api/authentication.interfaces.ts
+++ src/app/services/api/authentication.interfaces.ts
@@ -1,4 +1,5 @@
 export interface SimpleUserResponse {
   name: string
   username: string
+  token: string
 }
--- src/app/services/api/book-api.service.ts
+++ src/app/services/api/book-api.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { firstValueFrom } from "rxjs";
 import { SearchResponse } from "./book.interfaces";
 import { environment } from "../../../environments/environment";
+import { AuthenticationService } from "../authentication/authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookApi {
 
+  authorizationService = inject(AuthenticationService)
 
   constructor(private http: HttpClient) {
 
   }
 
   public async search(q: string, currentPage: number, pageSize: number): Promise<SearchResponse> {
-    return firstValueFrom(this.http.get<SearchResponse>(`${environment.API_URL}/api/books/search?title=${q}&page=${currentPage}&pageSize=${pageSize}`))
+
+    return firstValueFrom(this.http.get<SearchResponse>(`${environment.API_URL}/api/books/search?title=${q}&page=${currentPage}&pageSize=${pageSize}`, {
+      headers: { "Authorization": `Bearer ${this.authorizationService.credentials?.token}`}
+    }))
   }
 }
